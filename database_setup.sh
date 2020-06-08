37f65b85f44f832452802f1e788bab207105d5f4
ijyotimaurya150 <50417940+ijyotimaurya150@users.noreply.github.com>
2020-06-08 23:38:13 +0530

    Update database_setup.sh

--- database_setup.sh
+++ database_setup.sh
@@ -4,3 +4,4 @@ mysql -u root -p -e "CREATE USER 'client_gen'@'localhost' IDENTIFIED BY 'passwor
 mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS vpn;"
 mysql -u root -p -D vpn -e "CREATE TABLE clients(client_id INT NOT NULL, client_email VARCHAR(100) NOT NULL, client_key VARCHAR(50) NOT NULL, last_ip VARCHAR(50), config_file_name VARCHAR(20), date DATE NOT NULL, PRIMARY KEY (client_key));"
 mysql -u root -p -D vpn -e "GRANT INSERT  ON clients TO 'client_gen'@'localhost';"
+mysql -u root -p -D vpn -e "GRANT SELECT  ON clients TO 'client_gen'@'localhost';"
